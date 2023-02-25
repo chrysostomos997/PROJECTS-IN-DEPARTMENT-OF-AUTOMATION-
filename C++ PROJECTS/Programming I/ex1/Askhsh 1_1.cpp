b23973ee62726d9494e9ce9321deee9ada29d07b
Chrysostomos Intzempeis <70040026+chrysostomos997@users.noreply.github.com>
2023-02-25 17:23:13 +0200

    Delete C++ PROJECTS directory

--- C++ PROJECTS/Programming I/ex1/Askhsh 1_1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main ()
-{
-    cout<<"Tmima Automatismou";
-    cout<<"\nTmima \nAuto \nmatismou \n";
-    cout<<"Tmima\t Automatismou\n";
-    return 0;
-}
