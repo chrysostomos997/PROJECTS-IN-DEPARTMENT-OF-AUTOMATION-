0e5b99f60d0b16903c407f1936b30133128e4d21
Chrysostomos Intzempeis <xrisostomos1997@hotmail.com>
2023-03-19 12:54:37 +0200

    Update matalb_code.m

--- Fuzzy Logic/solutions/matalb_code.m
+++ Fuzzy Logic/solutions/matalb_code.m
@@ -1,5 +1,5 @@
 fuzzy
-A = [300 400 600 1000 900]; %Epithimita
-B = [350 350 600 950 950]; %Pragmatika (apo arxikopoihsh)
-error = (abs(A-B)./A)*100 %Sxetiko sfalma
-meso_sfalma = mean(error) %Meso Sfalma
+A = [300 400 600 1000 900]; %desired
+B = [350 350 600 950 950]; %real (from initialazation)
+error = (abs(A-B)./A)*100 %relative error
+mean_error = mean(error) %mean error
